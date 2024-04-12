8f3003a7a72b87d0bef0665be71909275502f2ed
Shiva1325 <30314900+Shiva1325@users.noreply.github.com>
2024-04-12 13:12:58 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,7 +1,8 @@
 import joblib as jb
 # import tensorflow as tf
+from keras.models import load_model
 def predict(data):
-    preprocess = jb.load("preprocessor.sav")
+    preprocess = load_model("preprocessor.sav")
     tensor_data = preprocess(data)
     # reg = jb.load("tensor_model.sav")
     # return reg.predict(tensor_data)
