a99f57d2b0fe2429c6f1e2b6efbbbbab2234d9ff
Shiva1325 <30314900+Shiva1325@users.noreply.github.com>
2024-04-12 14:20:42 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,7 +1,10 @@
 import joblib as jb
+import os
 # import tensorflow as tf
 from keras.models import load_model
 def predict(data):
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    print(files)
     # preprocess = load_model("preprocessor.sav
     preprocess = load_model("./preprocessor.keras")
     tensor_data = preprocess(data)
