5befc88ba25e87837774b6b64bdc13632bd17dd1
Shiva1325 <30314900+Shiva1325@users.noreply.github.com>
2024-04-12 13:42:40 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,11 +1,10 @@
 import joblib as jb
 # import tensorflow as tf
-import pickle
+from keras.models import load_model
 def predict(data):
-    # preprocess = load_model("preprocessor.sav")
-    # filename = 'preprocessor.sav'
-    # preprocess = pickle.load(open(filename, 'rb'))
-    # tensor_data = preprocess(data)
-    reg = jb.load("tensor_model.sav")
+    # preprocess = load_model("preprocessor.sav
+    preprocess = load_model('preprocessor.keras')
+    tensor_data = preprocess(data)
+    # reg = jb.load("tensor_model.sav")
     # return reg.predict(tensor_data)
-    return reg;
+    return tensor_data;
