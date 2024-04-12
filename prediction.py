d70089f0ca6653b23df9a07ba0424c5d95098d1e
Shiva1325 <30314900+Shiva1325@users.noreply.github.com>
2024-04-12 13:28:08 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -3,9 +3,9 @@ import joblib as jb
 import pickle
 def predict(data):
     # preprocess = load_model("preprocessor.sav")
-    filename = 'preprocessor.sav'
-    preprocess = pickle.load(open(filename, 'rb'))
-    tensor_data = preprocess(data)
-    # reg = jb.load("tensor_model.sav")
+    # filename = 'preprocessor.sav'
+    # preprocess = pickle.load(open(filename, 'rb'))
+    # tensor_data = preprocess(data)
+    reg = jb.load("tensor_model.sav")
     # return reg.predict(tensor_data)
-    return tensor_data;
+    return reg;
